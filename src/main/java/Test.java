cfab709299de05537e25ff8c63caa2724bfbf2f6
DondreFerguson <dondre94@gmail.com>
2025-02-17 14:23:49 -0500

    third commit

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -1,12 +1,10 @@
 public class Test {
     public static void main(String[] args) {
         System.out.println("Hello world");
-        System.out.println("this is the project version 2");
-        System.out.println("Hello world");
-        System.out.println("Hello world");
-        System.out.println("Hello world");
+        System.out.println("this is the project version 3");
+
 
 
     }
 }
-//done with version 2
+//done with version 3
