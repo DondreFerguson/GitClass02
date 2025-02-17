43156597f825b167fc82b0ab3d32161f4921ebc0
DondreFerguson <dondre94@gmail.com>
2025-02-17 14:20:21 -0500

    second commit

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -1,8 +1,12 @@
 public class Test {
     public static void main(String[] args) {
         System.out.println("Hello world");
-        System.out.println("this is the project version 1");
+        System.out.println("this is the project version 2");
+        System.out.println("Hello world");
+        System.out.println("Hello world");
+        System.out.println("Hello world");
+
 
     }
 }
-//done with version 1
+//done with version 2
